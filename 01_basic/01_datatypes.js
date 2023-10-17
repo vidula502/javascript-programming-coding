0caed1656794302490c62ba6ca07c6344e67ac33
vidula502 <49763017+vidula502@users.noreply.github.com>
2023-10-17 13:20:39 +0000

    container config files

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,16 @@
+let number = 10;
+let name = "Vidula";
+let age = 20;
+let gender = "Male";
+let country;
+let city = null;
+
+console.table([number, name, age, gender, country, city]);
+console.log(typeof 10);// number
+console.log(typeof null); // object
+console.log(typeof country); // undefined
+console.log(typeof city); // object
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
+console.log(typeof []); // object
+console.log(typeof {}); // object
