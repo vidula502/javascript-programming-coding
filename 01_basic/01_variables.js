981242c753f15cb98e1691f468c9faa4bc0db882
vidula502 <49763017+vidula502@users.noreply.github.com>
2023-10-17 13:12:46 +0000

    container config files

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 123567;
+let accountEmail = "vidula@gmail.com";
+var password = "345678";
+cityName = "Bangalore";
+
+console.table([accountID, accountEmail,password, cityName]);
+
+// Try to avoid using 'var' keyword while programming and prevent block and function scope.
