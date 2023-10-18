a47e615a282270d73d39fb59633787fc4d02065d
vidula502 <49763017+vidula502@users.noreply.github.com>
2023-10-18 01:31:23 +0000

    container config commit

---
+++ 01_basic/01_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score = "33";
+// console.log(typeof(score));
+
+// let scoreInNumber = Number(score);
+// console.log(scoreInNumber);
+
+// "33" => 33;
+//"33abc" => NaN
+// true => 1 false => 0
+
+let isLoggedIn = 1;
+
+console.log(isLoggedIn);
+
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+
+// "Vidula" => true
+// "" => 0
+// 0 => false 1 =>false
