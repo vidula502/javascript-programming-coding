24cf50e68ae0c0d61c444c5a3a111cdbbde4a20c
vidula502 <49763017+vidula502@users.noreply.github.com>
2023-10-18 10:42:34 +0000

    container config files

---
+++ 01_basic/01_camparison.js
@@ -0,0 +1,18 @@
+console.log(null > 0); //false
+console.log(null >= 0); //true
+console.log(null < 0); //false
+console.log(null == 0); //false
+console.log(null === 0); //false
+
+
+console.log("============================================");
+
+
+console.log(undefined > 0); //false
+console.log(undefined >= 0); //false
+console.log(undefined < 0); //false
+console.log(undefined == 0); //false
+console.log(undefined === 0); //false
+
+
+// "===" means strict check ...check with datatype also
--- 01_basic/01_conversionOperation.js
+++ 01_basic/01_conversionOperation.js
@@ -18,3 +18,40 @@ console.log(booleanLoggedIn);
 // "Vidula" => true
 // "" => 0
 // 0 => false 1 =>false
+
+let value = 3;
+let nagValue = -value;
+console.log(nagValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(3%2);
+
+
+// console.log("1" + 2); => 12
+// console.log(1 + "2"); => 12
+// console.log("2" + 2 + "1"); => 221
+// console.log(2 + "2" + 1); => 221
+// console.log(2 + 2 + 1 + "2"); => 52
+
+// console.log(3 + 4 * 5 % 3); => 5
+
+// console.log((3 + 4) * 5 % 3); =>
+
+// console.log(+true); => 1
+ 
+// console.log(-true); => -1
+
+// console.log(+""); => 0
+
+
+let x = 5;
+const y = x++;
+
+console.log("x :" + x + " y :" + y);  // x :6 y :5
+
+console.log(`x:${x}, y:${y}`); // x:6, y:5
